b960ce3c4362f15f5f31c83606d56da9fafe59fa
PabloLucas2005 <pablolucasfarias@gmail.com>
2022-10-22 19:16:12 -0300

    primeiro commit

---
+++ src/App.styles.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import styled from 'styled-components';
+
+
+
+export const Image = styled.div`
+background-image: url("https://amymhaddad.s3.amazonaws.com/morocco-blue.png");
+height: 400px;
+width: 100%; 
+
+`;
+
+
+export const Container = styled.div`
+    background-color: #16366c;
+    color: #797A81;
+    min-height: 100vh;
+`;
+
+export const Area = styled.div`
+    margin: auto;
+    max-width:980px;
+    padding:10px
+`;
+
+export const Header = styled.h1`
+    margin:0;
+    padding:0;
+    color:#FFF;
+    text-align: center;
+    border-bottom: 1px solid #444;
+    padding-bottom:20px;
+`;
+  
